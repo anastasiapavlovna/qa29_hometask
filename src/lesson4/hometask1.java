1a95b4856f054ec7ee1331d42d5c903c5c48dee0
anastasia <787kefir@gmail.com>
2024-12-05 14:08:44 -0500

    lesson4.hometask1

---
+++ src/lesson4/hometask1.java
@@ -0,0 +1,14 @@
+package lesson4;
+
+public class hometask1 {
+    public static void main(String[] args) {
+
+        int num = 10;
+
+        if (num % 2 == 0) {
+            System.out.println("chetnoe");
+        } else {
+            System.out.println("nechetnoe");
+        }
+    }
+}
