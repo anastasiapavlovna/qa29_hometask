ede962aa61f59b31e514d65c618e541e9a6104c5
anastasia <787kefir@gmail.com>
2024-12-09 12:54:03 -0500

    changes1

--- src/lesson4/Hometask1.java
+++ src/lesson4/Hometask1.java
@@ -2,15 +2,27 @@ package lesson4;
 
 import java.util.Arrays;
 
-public class hometask1 {
+public class Hometask1 {
     public static void main(String[] args) {
 
         int array[] = new int[]{3, 5, 6, 9, 2};
 
-        int array2[] = new int[]{1, 16, -1};
+        int array2[] = new int[]{1, 16, -1, 5};
 
         System.out.println(Arrays.toString(array));
         System.out.println(Arrays.toString(array2));
+
+
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array2.length; j++) {
+                if (array[i] == array2[j]) {
+                    System.out.println("the same element:" + array[i]);
+                }
+            }
+        }
+
+
 
         // TASK2
 
