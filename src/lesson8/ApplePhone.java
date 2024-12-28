f0b141ce4f17582f6968183cdac25b4a7ee24263
anastasia <787kefir@gmail.com>
2024-12-28 14:37:25 -0500

    lesson8-commmit1

---
+++ src/lesson8/ApplePhone.java
@@ -0,0 +1,10 @@
+package lesson8;
+
+public class ApplePhone extends Phone {
+
+    public void firmware(){
+        System.out.println("Check software update for Apple");
+    }
+
+
+}
---
+++ src/lesson8/ServiceQuality.java
@@ -0,0 +1,10 @@
+package lesson8;
+
+public class ServiceQuality {
+
+    public void check(Phone phone) {
+        phone.call();
+        phone.play();
+        phone.firmware();
+    }
+}
